2de03399f7d04dd58f1b2cb0dac3b50f330f483c
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-13 02:17:39 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,13 +24,14 @@ def output_array(array)
   end
 end
 
+#alternative solution
 #def output_array(array)
-  index = 0
-  while array[index] do
-    puts array[index]
-    index += 1 
-  end
-end
+#  index = 0
+#  while array[index] do
+#    puts array[index]
+#    index += 1 
+#  end
+#end
 
 def return_string_array(array)
   index = 0 
