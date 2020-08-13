fde0a98c017f846d3b33101b95798dd5279b8c88
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-13 02:20:45 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,49 @@
 # Write your methods here
+def loop_message_five_times(message)
+  counter = 0
+  while counter < 5 do
+    puts message
+    counter += 1
+  end
+end
+
+#n is the number of times you want to loop/repeat the message
+def loop_message_n_times(message, n)
+  counter = 0 
+  while counter < n do
+    puts message
+    counter += 1 
+  end
+end
+
+def output_array(array)
+  index = 0
+  while array[index] do
+    puts array[index]
+    index += 1 
+  end
+end
+
+#alternative solution
+#def output_array(array)
+#  count = 0
+#  while count < array.length do
+#    puts array[count]
+#    count += 1 
+#  end
+#end
+
+def return_string_array(array)
+  index = 0 
+  string_array = []
+  while array[index] do
+    string_array.push(array[index].to_s)
+    index += 1 
+  end
+#  while index < array.length do
+#   string_array.push(array[index].to_s)
+#    index += 1 
+#  end
+#  return string_array
+  string_array
+end
