4cdb6815d53b8a81ea386357fb2c1a000cb57b35
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-13 02:12:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,7 +16,12 @@ def loop_message_n_times(message, n)
   end
 end
 
-def output_array
+def output_array(array)
+  index = 0
+  while array[index] do
+    puts array[index]
+    counter += 1 
+  end
 end
 
 def return_string_array
